cbdf2f20808751e1924f756d5817c0fb3de51345
Angel Brand <angel.brand062@gmail.com>
2023-06-07 14:37:17 -0600

    instalacion de tailwind

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Encabezamiento from './componentes/compartidos/Encabezamiento';
+import Footer from './componentes/compartidos/Footer';
+import Principal from './componentes/compartidos/Principal';
 
 function App() {
   return (
     <div className="App">
-      
+      <Encabezamiento></Encabezamiento>
+      <Principal></Principal>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/componentes/compartidos/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+      <footer className="text-3xl font-bold underline">
+        <small>Desarrollado y disenado por Angel Brand. 2023. All rights reserved</small>
+      </footer>
+    );
+  }
+  
+  export default Footer;
